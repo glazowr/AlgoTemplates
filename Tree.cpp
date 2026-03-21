538679a2a9f742479d04e40e979d840574d16c00
glazowr <harrydresden576@gmail.com>
2026-03-21 18:02:56 +0530

    Update Tree.cpp
    Added inorder traversal methods using stack and Morris traversal.

--- Tree.cpp
+++ Tree.cpp
@@ -11,3 +11,40 @@ while (!q.empty()) {
         if (cur->right) q.push(cur->right);
     }
 }
+
+// Inorder traversal using stack and pointers
+vector<int> ans;
+stack<TreeNode*> st;
+TreeNode* cur = root;
+
+while (cur || !st.empty()) {
+
+    // 1. Go as left as possible
+    while (cur) {
+        st.push(cur);
+        cur = cur->left;
+    }
+
+    // 2. Process node
+    cur = st.top();
+    st.pop();
+    ans.push_back(cur->val);
+
+    // 3. Move to right subtree
+    cur = cur->right;
+}
+
+// morris traversal 
+while (true) {
+    if (cur != NULL) {
+        s.push(cur);
+        cur = cur->left;
+    }
+    else {
+        if (s.empty()) break;
+        cur = s.top();
+        s.pop();
+        l.push_back(cur->val);
+        cur = cur->right;
+    }
+}
